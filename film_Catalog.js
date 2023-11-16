f0142988448d809028a602e2a5ea1b5511a93b79
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-11-16 20:17:37 -0300

    Create film_Catalog.js

---
+++ film_Catalog.js
@@ -0,0 +1,5 @@
+var listaFilmes=['https://image.tmdb.org/t/p/original/7Ax9O1wlFL0Cjl8YLwuXDGthovf.jpg', 'https://tse3.mm.bing.net/th/id/OIP.QemjIrbzAAu7UwWUB054ZwHaJ4?rs=1&pid=ImgDetMain', 'https://tse3.mm.bing.net/th/id/OIP.RLu-LcBzvSIOcODgjQokHAHaK4?w=681&h=1000&rs=1&pid=ImgDetMain'];
+
+for (var i=0;i<3;i=i+1){
+	document.write('<img scr='+listaFilmes[i]+'>');
+}
